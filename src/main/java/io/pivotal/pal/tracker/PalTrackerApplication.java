311d536ecd5344cf4870cee4a475f88a7d2c47d0
ashoknitin <nitin.ashok2@gmail.com>
2019-03-14 11:20:12 +0530

    Database Integration-1

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -9,8 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
+
+    @Bean
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
